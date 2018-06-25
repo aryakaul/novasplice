a5fc1a3a13da6371cd6599df2024be664edbe306
Arya Kaul <akaul@tako.partners.org>
2018-06-25 10:00:51 -0400

    Update README and formatting

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
 # NovaSplice
-A tool to determine locations of potential novel intronic splicing given mutagenesis at those locations
+##### Developed by Arya Kaul (last updated on 2018-06-25)
+NovaSplice is a tool to determine locations of potential novel splicing given mutagenesis at those locations.
+
+
+## Requirements
+* Python3
+
+## Installation
+
+To use NovaSplice, first ensure that the requirements are correctly installed. Then run:
+```
+git clone https://github.com/aryakaul/novasplice.git
+cd novasplice/scripts
+python3 main.py --help
+```
+
+If all requirements are correctly installed you should see the following message:
+```
+usage: main.py [-h] -i FASTAFILE [-o OUTPUTPATH]
+
+Check the help flag
+
+optional arguments:
+    -h, --help            show this help message and exit
+    -i FASTAFILE, --fastaFile FASTAFILE 
+        Direct path to the fasta file being analyzed.
+    -o OUTPUTPATH, --outputPath OUTPUTPATH
+        Direct path to the desired output folder.
+```
+
+## Usage
+
+### NovaSplice
+Use NovaSplice by running:
+
+```
+python3 main.py -i ./FASTAFILE -o ./OUTPUTFOLDER/
+```
+
+The -i flag is required, and if no argument is specified for -o, then it will create a folder in the working directory with name `NovaSplice` and dump output files there. The bulk of the output is directed to stdout, and if that interests you, you should pipe the output of the command to a new file.
+
+If I pass NovaSplice a FASTA file with the following content:
+```
+>ex0
+GGAUUUUUUUUUUUUUUUAGU
+```
+
+An example of the first 13 lines of stdout is reproduced below:
+```
+Index of mutation: 0 Ref->Alt:G->A
+        Potential donor sites being analyzed
+        Potential branch points being analyzed
+                Potential branch point found
+        Potential acceptor sites being analyzed
+Index of mutation: 0 Ref->Alt:G->C
+        Potential donor sites being analyzed
+        Potential branch points being analyzed
+        Potential acceptor sites being analyzed
+Index of mutation: 0 Ref->Alt:G->U
+        Potential donor sites being analyzed
+        Potential branch points being analyzed
+        Potential acceptor sites being analyzed
+```
+As can be seen, NovaSplice searches every possible mutation event for the three potential sites explained below. The file `ex0.summary` in the created output folder has the following format:
+
+|LOCATION|A|C|G|T|
+|--------|-|-|-|-|
+|0       |2|2|3|R|
+
+An 'R' would denote the fact that this is the reference at this location. The numbers would correspond to the # of all possible splice sites generated at that position and with that nucleotide substitution. Note that multiple splice sites are possible at a given location and nucleotide since the distance between the donor splice site and the branch point is arbitrary. 
+
+## Description
+The following is how NovaSplice's algorithm currently works:
+1. Given some arbitrary RNA sequence of length *n*, generate every possible single nucleotide mutagenesis event. This leads to a set of possible mutated sequences of size ``3^{n}``, where each sequence has length *n*. Denote this set *R*
+2. For *s* in *R*:
+    * Check if mutation in *s* -> donor splice site
+    * Check if mutation in *s* -> branch point
+    * Check if mutation in *s* -> acceptor splice site
+3. Output those sequences and mutation events that lead to a splice site.
+
+A splice site is defined as a stretch of sequences having each of the following requirements:
+* A 'GU' sequence
+* A possible branch point some arbitrary position upstream of the 'GU'
+* A polypyrimidine tail following the potential branch point consisting of >13 pyrimidines and having more than 50% Uracil content
+* A 'AG' immediately after the polypyrimidine tail
+
+As of the writing of this. This procedure outputs a binary score (1/0) with no stochastic modeling. This is the next step in NovaSplice's development.
--- scripts/main.py
+++ scripts/main.py
@@ -45,26 +45,26 @@ def donorScore(dnaseq, index):
 
     #check if mutation leads to donor splice site
     if postDNA[0:2] == "GU":
-        print("Potential donor site found at index %s" % index)
+        print("\t\tPotential donor site found at index %s" % index)
         postpostDNA = postDNA[2:]
 
         #look for branch point
         for ntidx in range(len(postpostDNA)):
             if postpostDNA[ntidx] == "A":
-                print("Potential branch point found at index %s" % ntidx)
+                print("\t\tPotential branch point found at index %s" % ntidx)
                 branchpointpost = postpostDNA[ntidx+1:]
 
                 #look for polypyr tail
                 score, end = computePolyPyrProb(branchpointpost)
 
                 if score == 1:
-                    print("Potential poly-pyrimidine tail found at index %s" % end)
+                    print("\t\tPotential poly-pyrimidine tail found at index %s" % end)
                     polypyrpost = branchpointpost[end:]
 
                     #look for acceptor splice site
                     if polypyrpost[0:2] == "AG":
-                        print("Potential acceptor site found")
-                        print("Splice site found!")
+                        print("\t\tPotential acceptor site found")
+                        print("\t\tSplice site found!")
                         numberOfSpliceSites+=1
     return numberOfSpliceSites
 
@@ -90,7 +90,7 @@ def branchScore(dnaseq, index):
 
     #check if it can be a branch point
     if dnaseq[index]=="A":
-        print("Potential branch point found")
+        print("\t\tPotential branch point found")
         score, end = computePolyPyrProb(dnaseq[index+1:])
 
         #check polypyr tail exists
@@ -99,14 +99,14 @@ def branchScore(dnaseq, index):
 
             #check if acceptor site exists
             if polypyrpost[0:2] == "AG":
-                print("Potential acceptor site found")
+                print("\t\tPotential acceptor site found")
                 prebranch = dnaseq[:index]
 
                 #search for donor site
                 for i in range(len(prebranch)-1):
                     if prebranch[i:i+2]=="GU":
-                        print("Potential donor site found")
-                        print("Splice site found!")
+                        print("\t\tPotential donor site found")
+                        print("\t\tSplice site found!")
                         numberOfSpliceSites+=1
     return numberOfSpliceSites
 
@@ -116,25 +116,25 @@ def acceptorScore(dnaseq, index):
     #find sequence before potential acceptor splice site
     extractPrevDNA = dnaseq[:index+1]
     if extractPrevDNA[len(extractPrevDNA)-2:len(extractPrevDNA)] == "AG":
-        print("Potential acceptor splice site found")
+        print("\t\tPotential acceptor splice site found")
         polyPyrTailPlus = extractPrevDNA[:len(extractPrevDNA)-2][::-1]
         score, end = computePolyPyrProb(polyPyrTailPlus)
 
         #check for polypyr tail
         if score == 1:
-            print("Potential poly-pyrimidine tail found at index %s" % end)
+            print("\t\tPotential poly-pyrimidine tail found at index %s" % end)
             polypyrpre = polyPyrTailPlus[end:]
 
             #check for branch point
             if polypyrpre[0] == "A":
-                print("Potential branch point found")
+                print("\t\tPotential branch point found")
                 branchpre = polypyrpre[1:]
 
                 #check for acceptor site
                 for i in range(len(branchpre)-1):
                     if branchpre[i:i+2] == "UG":
-                        print("Potential donor site found")
-                        print("Splice site found!")
+                        print("\t\tPotential donor site found")
+                        print("\t\tSplice site found!")
                         numberOfSpliceSites+=1
     return numberOfSpliceSites
 
@@ -166,18 +166,18 @@ def main():
         mutatedNTs = generateMutagenesis(list(seqDict[seqids]))
         for mutations in mutatedNTs:
             print("Index of mutation: %s Ref->Alt:%s" % (mutations[1],mutations[2]))
-            print("Potential donor sites being analyzed")
+            print("\tPotential donor sites being analyzed")
             donorsplice = donorScore(mutations[0], mutations[1])
             mutations.append(donorsplice)
 
-            print("Potential branch points being analyzed")
+            print("\tPotential branch points being analyzed")
             branchsplice = branchScore(mutations[0], mutations[1])
             mutations.append(branchsplice)
 
-            print("Potential acceptor sites being analyzed")
+            print("\tPotential acceptor sites being analyzed")
             acceptsplice = acceptorScore(mutations[0], mutations[1])
             mutations.append(acceptsplice)
-        outputSpliceSumm(mutatedNTs, args.outputPath)
+        outputSpliceSumm(mutatedNTs, os.path.join(args.outputPath, seqids, '.summary'))
 
 
 
